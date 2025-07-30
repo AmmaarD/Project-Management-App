2d9576204db65780b95ea797236b4335722db9a9
Ammaar Doctor <ammaar.doctor@gmail.com>
2025-07-30 07:52:52 -0500

    redirected images to s3

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,10 +1,21 @@
 import type { NextConfig } from "next";
+import { hostname } from "os";
 
 const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "project-mgmt-s3-images.s3.us-east-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -77,7 +77,7 @@ const Sidebar = () => {
         </div>
         {/* Team */}
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray=200 px-8 py-4 dark:border-gray-700">
-          <Image src="/logo.png" alt="Logo" width={40} height={40}></Image>
+          <Image src="https://project-mgmt-s3-images.s3.us-east-1.amazonaws.com/logo.png" alt="Logo" width={40} height={40}></Image>
           <div>
             <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
               EDROH TEAM
