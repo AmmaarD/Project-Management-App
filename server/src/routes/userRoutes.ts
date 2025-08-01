ee823d5aa262b8e7ccda5b363fd13c3e152781b1
Ammaar Doctor <ammaar.doctor@gmail.com>
2025-08-01 13:03:59 -0500

    minor backend changes to create user

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -5,7 +5,7 @@ import { getUsers, postUser, getUser } from "../controllers/userController";
 const router = Router();
 
 router.get("/", getUsers);
-router.post("/", postUser);
+router.post("/create-user", postUser);
 router.get("/:cognitoId", getUser)
 
 export default router;
